1ed2626cf8feb0451f5a5ad1d3d96a90ed0232e3
Anderson Fernandes <142799982+Kabula21@users.noreply.github.com>
2026-01-07 13:40:27 -0300

    commit 3

--- server.js
+++ server.js
@@ -39,8 +39,12 @@ const pool = new Pool({
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
 
 pool.query('SELECT NOW()')
   .then(res => console.log('PostgreSQL conectado:', res.rows[0]))
